f86abb3621013fbca18c5371052182b354f72f6f
aaranyak <aaranyak.ghosh@gmail.com>
2022-07-31 10:48:37 +0530

    Started work on the c utility functions

--- gpu_functions/test_functions.c
+++ gpu_functions/test_functions.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "utilfuncs.h"
 int main(int argc, char const *argv[]) {
   int board[8][8] = {
     {1,2,3,4,5,6,7,8},
@@ -11,5 +11,12 @@ int main(int argc, char const *argv[]) {
     {32,31,30,29,28,27,26,25},
     {24,23,22,20,21,19,18,17},
   };
+  for (size_t y = 0; y < 8; y++) {
+    for (size_t x = 0; x < 8; x++) {
+      printf("%d", getPieceById(board[y][x]));
+      printf("%s", " ");
+    }
+    printf("%s", "\n");
+  }
   return 0;
 }
--- gpu_functions/utilfuncs.h
+++ gpu_functions/utilfuncs.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int getPieceById(int id) {
+  // Takes the piece id and returns the piece type.
   if (id == 1 || id == 8 || id == 24 || id == 17) {
     // If piece is a rook.
     return 1;
@@ -8,5 +9,24 @@ int getPieceById(int id) {
   else if (id == 2 || id == 7 || id == 23 || id == 18) {
     // If piece is a knight.
     return 2;
+  }
+  else if (id == 3 || id == 6 || id == 22 || id == 19) {
+    // If piece is a bishop.
+    return 3;
+  }
+  else if (id == 4 || id == 20) {
+    // If piece is a king.
+    return 4;
+  }
+  else if (id == 5 || id == 21) {
+    // If piece is a queen.
+    return 5;
+  }
+  else if ((id >= 9 && id <= 16) || (id <= 32 && id >= 25)) {
+    // If piece is a pawn.
+    return 6;
+  }
+  else {
+    return 0;
   }
 }
