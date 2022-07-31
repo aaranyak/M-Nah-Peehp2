95e15615a1a482e01fdc76d215f2d7aae77d057c
aaranyak <aaranyak.ghosh@gmail.com>
2022-07-31 11:44:02 +0530

    Tested some stuff to see if it works.

--- gpu_functions/test_functions.c
+++ gpu_functions/test_functions.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include "utilfuncs.h"
+int** testarrayer(int board[8][8]) {
+  int** retval;
+  for (size_t y = 0; y < 8; y++) {
+    for (size_t x = 0; x < 8; x++) {
+      retval[y][x] = board[y][x] + 1;
+    }
+  }
+  return retval;
+}
+
 int main(int argc, char const *argv[]) {
   int board[8][8] = {
     {1,2,3,4,5,6,7,8},
@@ -11,18 +21,13 @@ int main(int argc, char const *argv[]) {
     {32,31,30,29,28,27,26,25},
     {24,23,22,20,21,19,18,17},
   };
-  int newtester = testarrayer();
+  int** newtester = testarrayer(board);
   for (size_t y = 0; y < 8; y++) {
     for (size_t x = 0; x < 8; x++) {
-      printf("%d", newtester[y][x]);
+      printf("%d", getPieceById(board[y][x]));
       printf("%s", " ");
     }
     printf("%s", "\n");
   }
   return 0;
-}
-
-int testarrayer() {
-  int b[8][8];
-  return b;
 }
